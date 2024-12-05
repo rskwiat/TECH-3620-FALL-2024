da4eaed7f4f3d1ead87016c0a3598671fc171cda
Robert Skwiat <rob@robertskwiat.com>
2024-12-05 10:09:30 -0500

    starting rework to fix all issues?

--- schemas/forgotPasswordSchema.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-
-export const ForgotPasswordSchema = z.object({
-  email: z.string().email({ message: "Invalid email address" }),
-});
-
-export type ForgotPasswordType = z.infer<typeof ForgotPasswordSchema>;
